f9bd1f1d18e42bd4a7aaa454a8c0f1560059b984
Brian Osborn <bosborn@gmail.com>
2021-03-25 13:56:00 -0600

    start of wkt parsing

---
+++ src/main/java/mil/nga/proj/wkt/CRSOlderTokens.java
@@ -0,0 +1,24 @@
+package mil.nga.proj.wkt;
+
+/**
+ * Coordinate Reference System Tokens from older WKT versions
+ * 
+ * @author osbornb
+ */
+public class CRSOlderTokens {
+
+	public static final String COMPDCS = "COMPDCS";
+
+	public static final String FITTED_CS = "FITTED_CS";
+
+	public static final String GEOCCS = "GEOCCS";
+
+	public static final String GEOGCS = "GEOGCS";
+
+	public static final String LOCAL_CS = "LOCAL_CS";
+
+	public static final String PROJCS = "PROJCS";
+
+	public static final String VERT_CS = "VERT_CS";
+
+}
---
+++ src/main/java/mil/nga/proj/wkt/CoordinateReferenceSystem.java
@@ -0,0 +1,5 @@
+package mil.nga.proj.wkt;
+
+public class CoordinateReferenceSystem {
+
+}
